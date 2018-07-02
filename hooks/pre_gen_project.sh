fc81413a51a0285c9ac3ca613fbfb7c67103d524
jayven <kassarar@gmail.com>
2018-07-02 11:12:32 +0800

    Add Makefile target to re-generate models

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -3,6 +3,9 @@
 # Check docker installation.
 command -v docker > /dev/null 2>&1 || { echo >&2 "docker is required but not installed. Aborting"; exit 1; }
 
+# Check sqlw-mysql installation.
+command -v sqlw-mysql > /dev/null 2>&1 || { echo > &2 "sqlw-mysql is required but not installed. Aborting"; exit 1; }
+
 # Check alembic installation.
 command -v alembic > /dev/null 2>&1 || { echo >&2 "alembic is required but not installed. Aborting"; exit 1; }
 

8bd6159ce02af2e1cf2051ef915bad99cfb0a03e
jayven <kassarar@gmail.com>
2018-07-23 16:03:18 +0800

    将初始化 service 中间件的逻辑移到 services package 中

--- {{cookiecutter.app_name}}/init.go
+++ {{cookiecutter.app_name}}/init.go
@@ -1,14 +1,12 @@
 package {{ cookiecutter.app_name }}
 
 import (
-	"context"
 	"database/sql"
 
 	"%%baseimport%%/endpoints"
 	"%%baseimport%%/services"
 	"%%baseimport%%/vars"
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 	"github.com/huangjunwen/platform-kit/svc"
 	"github.com/rs/zerolog"
 )
@@ -27,32 +25,8 @@ func Initialize(
 	vars.DB = db
 
 	// 服务
-	vars.SvcServer = libsvc.DecorateServer(svcServer, func(h libsvc.ServiceHandler) libsvc.ServiceHandler {
-		return func(ctx context.Context, method libsvc.Method, input, output interface{}) error {
-			// 创建一个 logger 的拷贝
-			l := vars.Logger.With().Logger()
-
-			// 是否有传递过来的 reqid
-			reqID := libsvc.Passthru(ctx)[vars.ReqIDFieldName]
-			if reqID != "" {
-				l.UpdateContext(func(c zerolog.Context) zerolog.Context {
-					return c.Str(vars.ReqIDFieldName, reqID)
-				})
-			}
-
-			return h(l.WithContext(ctx), method, input, output)
-		}
-	})
-	vars.SvcClient = libsvc.DecorateClient(svcClient, func(h libsvc.ServiceHandler) libsvc.ServiceHandler {
-		return func(ctx context.Context, method libsvc.Method, input, output interface{}) error {
-			// Context 中若有 reqID，则传递过去
-			reqID := middleware.GetReqID(ctx)
-			if reqID != "" {
-				ctx = libsvc.WithPassthru(ctx, map[string]string{vars.ReqIDFieldName: reqID})
-			}
-			return h(ctx, method, input, output)
-		}
-	})
+	vars.SvcServer = svcServer
+	vars.SvcClient = svcClient
 
 	// TODO 初始化其它全局变量
 	// ...
--- {{cookiecutter.app_name}}/services/services.go
+++ {{cookiecutter.app_name}}/services/services.go
@@ -1,6 +1,43 @@
 package services
 
+import (
+	"context"
+
+	"%%baseimport%%/vars"
+	"github.com/go-chi/chi/middleware"
+	"github.com/huangjunwen/platform-kit/svc"
+	"github.com/rs/zerolog"
+)
+
 // InitServices 在全局变量初始化完成后进行
 func InitServices() error {
+	// 给 SvcServer/SvcClient 安装中间件
+	vars.SvcServer = libsvc.DecorateServer(vars.SvcServer, func(h libsvc.ServiceHandler) libsvc.ServiceHandler {
+		return func(ctx context.Context, method libsvc.Method, input, output interface{}) error {
+			// 创建一个 logger 的拷贝
+			l := vars.Logger.With().Logger()
+
+			// 是否有传递过来的 reqid
+			reqID := libsvc.Passthru(ctx)[vars.ReqIDFieldName]
+			if reqID != "" {
+				l.UpdateContext(func(c zerolog.Context) zerolog.Context {
+					return c.Str(vars.ReqIDFieldName, reqID)
+				})
+			}
+
+			return h(l.WithContext(ctx), method, input, output)
+		}
+	})
+	vars.SvcClient = libsvc.DecorateClient(vars.SvcClient, func(h libsvc.ServiceHandler) libsvc.ServiceHandler {
+		return func(ctx context.Context, method libsvc.Method, input, output interface{}) error {
+			// Context 中若有 reqID，则传递过去
+			reqID := middleware.GetReqID(ctx)
+			if reqID != "" {
+				ctx = libsvc.WithPassthru(ctx, map[string]string{vars.ReqIDFieldName: reqID})
+			}
+			return h(ctx, method, input, output)
+		}
+	})
+
 	return nil
 }
